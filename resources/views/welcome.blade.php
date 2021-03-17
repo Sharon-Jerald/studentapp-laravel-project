d153578d7603c01018ec6b86cf66eaabfd95a884
Sharon-Jerald <sharuz9799@gmail.com>
2021-03-17 11:56:35 +0530

    student app

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>\
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student</title>
+</head>
+<body>
+   <center> <div class="container">
+    <div class="row">
+    <div class="col col-12 col-xs-4 col-sm-4 col-md-4 col-lg-4">
+    <table class="table table-borderless">
+    <tr>
+        <td>Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Roll No.</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Admission No.</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>College Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><button class="btn btn-success">Submit</button></td>
+    </tr>
+    </table>
+    </div>
+    </div>
+    </div></center>
+</body>
+</html>
